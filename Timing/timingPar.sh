0ac503a7d58fff7a5297cbe4203f03525778d119
zzzaJ <dino.bossi4@gmail.com>
2017-09-02 18:10:36 +0200

    Fixed bash scripts

--- Timing/timingPar.sh
+++ Timing/timingPar.sh
@@ -5,28 +5,29 @@
 java InputFileWriter <<EOF
 50000
 EOF
-echo "n = 50000" >> timingsSeq
-echo "fsize = 21" >> timingsSeq
+echo 
+echo "n = 50000" >> timingsPar
+echo "fsize = 3" >> timingsPar
 for i in 1 2 3 4 5 6
 do
-java Medfiltseq >> timingsSeq<<EOF
+java Medfiltpar >> timingsPar<<EOF
+outfile 3 out
+EOF
+done
+
+echo "fsize = 11" >> timingsPar
+for i in 1 2 3 4 5 6
+do
+java Medfiltpar >> timingsPar<<EOF
+outfile 11 out
+EOF
+done
+
+echo "fsize = 21" >> timingsPar
+for i in 1 2 3 4 5 6
+do
+java Medfiltpar >> timingsPar<<EOF
 outfile 21 out
-EOF
-done
-
-echo "fsize = 75" >> timingsSeq
-for i in 1 2 3 4 5 6
-do
-java Medfiltseq >> timingsSeq<<EOF
-outfile 99 out
-EOF
-done
-
-echo "fsize = 151" >> timingsSeq
-for i in 1 2 3 4 5 6
-do
-java Medfiltseq >> timingsSeq<<EOF
-outfile 151 out
 EOF
 done
 
@@ -35,28 +36,28 @@ done
 java InputFileWriter <<EOF
 100000
 EOF
-echo "n = 100000" >> timingsSeq
-echo "fsize = 21" >> timingsSeq
+echo "n = 100000" >> timingsPar
+echo "fsize = 3" >> timingsPar
 for i in 1 2 3 4 5 6
 do
-java Medfiltseq >> timingsSeq<<EOF
+java Medfiltpar >> timingsPar<<EOF
+outfile 3 out
+EOF
+done
+
+echo "fsize = 11" >> timingsPar
+for i in 1 2 3 4 5 6
+do
+java Medfiltpar >> timingsPar<<EOF
+outfile 11 out
+EOF
+done
+
+echo "fsize = 21" >> timingsPar
+for i in 1 2 3 4 5 6
+do
+java Medfiltpar >> timingsPar<<EOF
 outfile 21 out
-EOF
-done
-
-echo "fsize = 75" >> timingsSeq
-for i in 1 2 3 4 5 6
-do
-java Medfiltseq >> timingsSeq<<EOF
-outfile 99 out
-EOF
-done
-
-echo "fsize = 151" >> timingsSeq
-for i in 1 2 3 4 5 6
-do
-java Medfiltseq >> timingsSeq<<EOF
-outfile 151 out
 EOF
 done
 
@@ -65,28 +66,28 @@ done
 java InputFileWriter <<EOF
 500000
 EOF
-echo "n = 500000" >> timingsSeq
-echo "fsize = 21" >> timingsSeq
+echo "n = 500000" >> timingsPar
+echo "fsize = 3" >> timingsPar
 for i in 1 2 3 4 5 6
 do
-java Medfiltseq >> timingsSeq<<EOF
+java Medfiltpar >> timingsPar<<EOF
+outfile 3 out
+EOF
+done
+
+echo "fsize = 11" >> timingsPar
+for i in 1 2 3 4 5 6
+do
+java Medfiltpar >> timingsPar<<EOF
+outfile 11 out
+EOF
+done
+
+echo "fsize = 21" >> timingsPar
+for i in 1 2 3 4 5 6
+do
+java Medfiltpar >> timingsPar<<EOF
 outfile 21 out
-EOF
-done
-
-echo "fsize = 75" >> timingsSeq
-for i in 1 2 3 4 5 6
-do
-java Medfiltseq >> timingsSeq<<EOF
-outfile 99 out
-EOF
-done
-
-echo "fsize = 151" >> timingsSeq
-for i in 1 2 3 4 5 6
-do
-java Medfiltseq >> timingsSeq<<EOF
-outfile 151 out
 EOF
 done
 
@@ -95,27 +96,27 @@ done
 java InputFileWriter <<EOF
 1000000
 EOF
-echo "n = 1000000" >> timingsSeq
-echo "fsize = 21" >> timingsSeq
+echo "n = 1000000" >> timingsPar
+echo "fsize = 3" >> timingsPar
 for i in 1 2 3 4 5 6
 do
-java Medfiltseq >> timingsSeq<<EOF
+java Medfiltpar >> timingsPar<<EOF
+outfile 3 out
+EOF
+done
+
+echo "fsize = 11" >> timingsPar
+for i in 1 2 3 4 5 6
+do
+java Medfiltpar >> timingsPar<<EOF
+outfile 11 out
+EOF
+done
+
+echo "fsize = 21" >> timingsPar
+for i in 1 2 3 4 5 6
+do
+java Medfiltpar >> timingsPar<<EOF
 outfile 21 out
-EOF
-done
-
-echo "fsize = 75" >> timingsSeq
-for i in 1 2 3 4 5 6
-do
-java Medfiltseq >> timingsSeq<<EOF
-outfile 99 out
-EOF
-done
-
-echo "fsize = 151" >> timingsSeq
-for i in 1 2 3 4 5 6
-do
-java Medfiltseq >> timingsSeq<<EOF
-outfile 151 out
 EOF
 done
